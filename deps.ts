47f039e8e5c6e916d30c2eaf79783f37c0f705c7
sho621bake <sho6217@gmail.com>
2022-06-07 14:29:51 +0900

    helpersをインポート

--- deps.ts
+++ deps.ts
@@ -3,6 +3,7 @@ export {
     Router,
     Context,
     Status,
+    helpers,
 } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
 export type { RouterContext } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
 
