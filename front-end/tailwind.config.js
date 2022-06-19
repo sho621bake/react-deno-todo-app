dfe0c0a918972038ddae057ed727d27286b219ac
sho621bake <sho6217@gmail.com>
2022-06-19 21:05:55 +0900

    Tailwind CSS 設定変更

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,49 @@
+// prettier-ignore
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        },
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            'white': '#ffffff',
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+            'red': {
+                50: '#fef2f2',
+                100:'#fee2e2',
+                200:'#fecaca',
+                300:'#fca5a5',
+                400:'#f87171',
+                500:'#ef4444',
+                600:'#dc2626',
+                700:'#b91c1c',
+                800:'#991b1b',
+                900:'#7f1d1d'
+            }
+        },
     },
     plugins: [require('preline/plugin')],
 }
