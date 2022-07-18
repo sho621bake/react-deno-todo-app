9a82e8d4c24f445b0524252819bd1e96a13c1e54
sho621bake <sho6217@gmail.com>
2022-07-18 18:45:32 +0900

    CORS対応

--- back-end/deps.ts
+++ back-end/deps.ts
@@ -7,6 +7,8 @@ export {
 } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
 export type { RouterContext } from 'https://deno.land/x/oak@v6.5.0/mod.ts'
 
+export { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts'
+
 export {
     MySQLConnector,
     DataTypes,
--- back-end/server.ts
+++ back-end/server.ts
@@ -1,16 +1,18 @@
 import { Application, yellow, magenta } from './deps.ts'
 import { router } from './router.ts'
 import { logger } from './middleware/logger.ts'
+import { oakCors } from './deps.ts'
 
 const app = new Application()
 
+app.use(oakCors())
 app.use(logger)
 app.use(router.routes())
 app.use(router.allowedMethods())
 app.addEventListener('listen', ({ hostname, port }) => {
     console.log(
         yellow('Start listening on :') +
-            magenta(`${hostname ?? 'localhost'}:${port}`),
+            magenta(`${hostname ?? 'http://localhost'}:${port}`),
     )
 })
 
